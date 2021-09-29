a10b7c205137b158c80f42ff08bff2a85e62e056
Kwame Osae Afari <kayfari20@gamil.com>
2021-09-29 17:26:33 +0000

    first push heroku

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
+//import { HomePageContainer } from './homepage.styles';
+
 
 
 const HomePage = () => (
