88a8cd83b17e1367127817569485cfbc3699d051
Kwame Osae Afari <kayfari20@gamil.com>
2021-10-01 16:23:37 +0000

    converted collection component to styled-component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -3,7 +3,10 @@ import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item /collection-item.component'
 import { selectCollection } from '../../redux/shop/shop.selectors';
-import { CollectionItemsContainer, CollectionPageContainer, CollectionTitle } from './collection.styles';
+import { 
+  CollectionItemsContainer, 
+  CollectionPageContainer, 
+  CollectionTitle } from './collection.styles';
 
 //import './collection.styles.scss';
 
